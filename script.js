0e121c26627cf27c0e45c98ebabc03aa174ac534
PEROVAS <lucas_pacheco5@estudante.sesisenai.org.br>
2025-03-20 09:22:46 -0300

    add

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+let numero = prompt('Digite seu número:');
+function verificarPar(numero){
+    if(numero % 2 === 0 ){
+        console.log(numero + " é igual a par");
+    }else{
+        console.log(numero + " é igual a ímpar");
+    }
+}
+verificarPar(numero);
